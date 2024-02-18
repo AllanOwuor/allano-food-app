939d64d2944c099f35f59dfb34016ac0541a73d6
Owuor Allantess Odila <144439690+AllanOwuor@users.noreply.github.com>
2024-02-18 13:11:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+        card.addEventListener('click', function() {
+            const cardImageSrc = card.querySelector('img').getAttribute('src');
+            const logoImage = document.querySelector('.logo img');
+            logoImage.setAttribute('src', cardImageSrc);
+        });
+    });
+
     const moonIcon = document.querySelector('.fa-moon');
 
     moonIcon.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
     });
 });
+
+function myfunc() {
+    alert("Selection added to cart");
+}
